c728ce296fbd8a57d9cd13e7b9f0df5084d384bc
huyanping <hypxm@qq.com>
2015-11-13 19:55:03 +0800

    add Master

---
+++ src/Master.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @author Jenner <hypxm@qq.com>
+ * @license https://opensource.org/licenses/MIT MIT
+ * @datetime: 2015/11/13 16:37
+ */
+
+namespace React\Multi;
+
+
+use Jenner\SimpleFork\FixedPool;
+use React\EventLoop\LoopInterface;
+
+class Master
+{
+    /**
+     * @var LoopInterface
+     */
+    protected $loop;
+
+    /**
+     * @var int process count
+     */
+    protected $count;
+
+    /**
+     * @param LoopInterface $loop
+     * @param int $count
+     */
+    public function __construct(LoopInterface $loop, $count = 10)
+    {
+        $this->loop = $loop;
+        $this->count = $count;
+    }
+
+    /**
+     * start multi loop
+     */
+    public function start()
+    {
+        $loop = $this->loop;
+        $pool = new FixedPool(function () use ($loop) {
+            $loop->run();
+        }, $this->count);
+
+        $pool->start();
+        $pool->keep(true);
+    }
+}
