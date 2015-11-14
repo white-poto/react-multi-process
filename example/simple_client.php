d70fab8d90ef603c42fff1aa5e105edb3a62e191
huyanping <hypxm@qq.com>
2015-11-14 09:15:07 +0800

    tests

--- example/simple_client.php
+++ example/simple_client.php
@@ -4,3 +4,19 @@
  * @license https://opensource.org/licenses/MIT MIT
  * @datetime: 2015/11/13 20:21
  */
+
+$loop = React\EventLoop\Factory::create();
+
+$dnsResolverFactory = new React\Dns\Resolver\Factory();
+$dns = $dnsResolverFactory->createCached('8.8.8.8', $loop);
+
+$connector = new React\SocketClient\Connector($loop, $dns);
+
+$connector->create('127.0.0.1', 80)->then(function (React\Stream\Stream $stream) use($loop) {
+    $loop->addReadStream($stream, function($data){
+        echo $data . PHP_EOL;
+    });
+    $stream->close();
+});
+
+$loop->run();
