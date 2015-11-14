d29c8b4dfd2c00cb6eaa083ad72768ddda00888e
huyanping <hypxm@qq.com>
2015-11-14 09:20:26 +0800

    tests

--- example/simple_client.php
+++ example/simple_client.php
@@ -14,10 +14,9 @@ $dns = $dnsResolverFactory->createCached('8.8.8.8', $loop);
 $connector = new React\SocketClient\Connector($loop, $dns);
 
 $connector->create('127.0.0.1', 80)->then(function (React\Stream\Stream $stream) use($loop) {
-    $loop->addReadStream($stream->stream, function($data){
+    $stream->on('data', function($data){
         echo $data . PHP_EOL;
     });
-    $stream->close();
 });
 
 $loop->run();
