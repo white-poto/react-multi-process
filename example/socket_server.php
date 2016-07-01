948f15749dabc6039251ae4c12c770291aec399a
huyanping <hypxm@qq.com>
2016-07-01 09:52:03 +0800

    add socket server

---
+++ example/socket_server.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2016/7/1
+ * Time: 9:48
+ */
+
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+
+class Handler implements \React\Multi\Socket\HandlerInterface {
+
+    /**
+     * handle request
+     *
+     * @param $conn
+     * @param \React\EventLoop\LoopInterface $loop
+     * @return mixed
+     */
+    public function handle($conn, \React\EventLoop\LoopInterface $loop)
+    {
+        var_dump(fread($conn));
+
+    }
+}
+
+$config = new \React\Multi\Socket\ServerConfig();
+
+
+$server = new \React\Multi\Socket\Server($config, new Handler());
+$server->start();
